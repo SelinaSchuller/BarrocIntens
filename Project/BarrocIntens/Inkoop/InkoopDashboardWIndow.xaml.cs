b6489647d773e87d1e3bef1ce852d18b92615c6c
d294626 <117263785+d294626@users.noreply.github.com>
2024-11-04 12:34:24 +0100

    InkoopDashboard pagina gemaakt

---
+++ Project/BarrocIntens/Inkoop/InkoopDashboardWIndow.xaml.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace BarrocIntens.Inkoop
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class InkoopDashboardWindow : Window
+    {
+        public InkoopDashboardWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ProductButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BestelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZoekButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NieuwProductButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
