d1a42acd36d2528018a7e1bca319fd501ccf55f5
d294626 <117263785+d294626@users.noreply.github.com>
2024-11-07 11:51:24 +0100

    Navigatie naar Producten Aanmaak Pagina

--- Project/BarrocIntens/Inkoop/InkoopDashboardWIndow.xaml.cs
+++ Project/BarrocIntens/Inkoop/InkoopDashboardWIndow.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using BarrocIntens.Onderhoud;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,29 +27,15 @@ namespace BarrocIntens.Inkoop
         public InkoopDashboardWindow()
         {
             this.InitializeComponent();
+            contentFrame.Navigate(typeof(ProductenPage));
         }
 
         private void ProductButton_Click(object sender, RoutedEventArgs e)
         {
-
+            contentFrame.Navigate(typeof(ProductenPage));
         }
 
         private void BestelButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ZoekButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void FilterButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void NieuwProductButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
