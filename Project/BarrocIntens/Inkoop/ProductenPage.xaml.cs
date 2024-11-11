12add8667c78f9ea14e3d850cb4961e0ea43ad74
d294626 <117263785+d294626@users.noreply.github.com>
2024-11-11 12:21:05 +0100

    Product verwijderen toegevoegt

--- Project/BarrocIntens/Inkoop/ProductenPage.xaml.cs
+++ Project/BarrocIntens/Inkoop/ProductenPage.xaml.cs
@@ -9,9 +9,11 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -56,9 +58,47 @@ namespace BarrocIntens.Inkoop
 
         }
 
-        private void VerwijderButton_Click(object sender, RoutedEventArgs e)
+        private async void VerwijderButton_Click(object sender, RoutedEventArgs e)
         {
+            var button = sender as Button;
+            if (button != null)
+            {
+                var product = button.DataContext as Products;
 
+                if (product != null)
+                {
+                    bool isDeleted = await DeleteProductFromDatabase(product);
+
+                    if (isDeleted)
+                    {
+                        var productList = ProductListView.ItemsSource as ObservableCollection<Products>;
+                        if (productList != null)
+                        {
+                            productList.Remove(product);
+                        }
+                    }
+                }
+            }
         }
+
+        private async Task<bool> DeleteProductFromDatabase(Products product)
+        {
+            using (var db = new AppDbContext())
+            {
+                var productToDelete = await db.Products
+                                                    .FirstOrDefaultAsync(p => p.Id == product.Id);
+
+                if (productToDelete != null)
+                {
+                    db.Products.Remove(productToDelete);
+                    await db.SaveChangesAsync();
+                    ProductListView.ItemsSource = db.Products.Include(p => p.Category).OrderBy(p => p.Id).ToList();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
